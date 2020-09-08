19ce7d2791a956bc2846e296eaac7bb66b151843
Adrian Bartsch <Adrian.Bartsch@marxkrontal.com>
2020-09-08 14:30:36 +0200

    bugfix

--- public/app/core/components/Branding/Branding.tsx
+++ public/app/core/components/Branding/Branding.tsx
@@ -41,7 +41,7 @@ export class Branding {
   static AppTitle = 'iris';
   static LoginTitle = 'Welcome to iris';
   static GetLoginSubTitle = () => {
-    const slogans = ["MKP GmbH",];
+    const slogans = ['MKP·GmbH'];
     const count = slogans.length;
     return slogans[Math.floor(Math.random() * count)];
   };
