a704ebcbb9d7cab2905cecc17ca415bae744f0de
Adrian Bartsch <Adrian.Bartsch@marxkrontal.com>
2020-09-11 09:08:17 +0200

    iris version

--- pkg/api/index.go
+++ pkg/api/index.go
@@ -360,7 +360,7 @@ func (hs *HTTPServer) setIndexViewData(c *models.ReqContext) (*dtos.IndexViewDat
 		})
 	}
 
-	helpVersion := fmt.Sprintf(`%s v%s (%s)`, setting.ApplicationName, setting.BuildVersion, setting.BuildCommit)
+	helpVersion := fmt.Sprintf(`%s v%s`, setting.ApplicationName, setting.BuildVersion)
 	if hs.Cfg.AnonymousHideVersion && !c.IsSignedIn {
 		helpVersion = setting.ApplicationName
 	}
